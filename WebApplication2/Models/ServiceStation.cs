a575078305db8f9696cc4e81ac4042a99b0713ee
Juan Carlos Santamaria Villegas <jsantamv@live.com>
2019-11-02 18:39:04 -0600

    Add project files.

---
+++ WebApplication2/Models/ServiceStation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class ServiceStation
+    {
+        //OrderDate Region  Rep Item    Units Unit Cost Total
+        public DateTime OrderDate { get; set; }
+        public string Region { get; set; }
+        public string Rep { get; set; }
+        public string Item { get; set; }
+        public string Units { get; set; }
+        public decimal UnitCost { get; set; }
+        public decimal Total { get; set; }
+    }
+}
---
+++ mvcPrueba/wwwroot/js/Scripts/ejemplovue.js
@@ -0,0 +1,7 @@
+
+  var app = new Vue({
+    el: "#app",
+    data: {
+        messege: "Hola Mundo2"
+    }
+})
